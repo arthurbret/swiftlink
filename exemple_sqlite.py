6c7b7eb3bf401f593627cf0d0836f9de883ec8ad
Arthur <arthur.bret@edu.ece.fr>
2023-12-18 09:15:16 +0100

    Fonctions principales SQLite

--- exemple_sqlite.py
+++ exemple_sqlite.py
@@ -1,15 +1,26 @@
+# Initialisation de la base de données sqlite3
 import sqlite3
 connection = sqlite3.connect('database.db')
 print(connection.total_changes)
 cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)')
+cursor.execute('CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY, prenom TEXT, nom TEXT, email TEXT UNIQUE, tel TEXT UNIQUE, profilepic TEXT)')
 
-cursor.execute('INSERT INTO users VALUES (1, "John", 22)')
-cursor.execute('INSERT INTO users VALUES (2, "Jane", 21)')
-cursor.execute('INSERT INTO users VALUES (3, "Mike", 23)')
-connection.commit()
+# Fonction création d'un contact
+def create_contact(prenom, nom, email, tel, profilepic):
+    cursor.execute('INSERT INTO contacts (prenom, nom, email, tel, profilepic) VALUES (?, ?, ?, ?, ?)', (prenom, nom, email, tel, profilepic))
+    connection.commit()
 
-cursor.execute('SELECT * FROM users')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+# Fonction suppression d'un contact
+def delete_contact(id):
+    cursor.execute('DELETE FROM contacts WHERE id = ?', (id,))
+    connection.commit()
+
+# Fonction modification d'un contact
+def update_contact(id, prenom, nom, email, tel, profilepic):
+    cursor.execute('UPDATE contacts SET prenom = ?, nom = ?, email = ?, tel = ?, profilepic = ? WHERE id = ?', (prenom, nom, email, tel, profilepic, id))
+    connection.commit()
+
+# Fonction recherche d'un contact
+def search_contact(nom):
+    cursor.execute('SELECT * FROM contacts WHERE nom = ?', (nom,))
+    return cursor.fetchall()
