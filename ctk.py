b8adbf69e4d5bf9fb886e969becb8e0bb46617c9
franck <franck.ramaherison@edu.ece.fr>
2023-12-19 10:22:57 +0100

    a

--- ctk.py
+++ ctk.py
@@ -2,6 +2,9 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
 from SQLite_func import create_contact
+from SQLite_func import delete_contact
+from SQLite_func import update_contact
+from SQLite_func import search_contact
 from SQLite_func import get_all_contacts
 
 ctk.set_default_color_theme("dark-blue")
@@ -10,27 +13,60 @@ ctk.set_default_color_theme("dark-blue")
 def change_tab_Rechercher():
     tabview.set("Rechercher")
 
+def clear_fields():
+    prenom_var.set("")
+    nom_var.set("")
+    email_var.set("")
+    telephone_var.set("")
+    photo_var.set("")
+    print("Clearing fields")
+
 def ajouter_contact():
     nom = nom_var.get()
     prenom = prenom_var.get()
     email = email_var.get()
     tel = telephone_var.get()
     photo = photo_var.get()
+    clear_fields()
 
     create_contact(prenom, nom, email, tel, photo)
     update_contacts_list()
 
 def supprimer_contact():
-    pass
+    selected_item = tree.selection()
+    if selected_item:
+        contact_id = tree.item(selected_item)['values'][0]
+        print(contact_id)
+        delete_contact(contact_id)
+        update_contacts_list()
+
 
 def modifier_contact():
-    pass
+    selected_item = tree.selection()
+    if selected_item:
+        contact_id = tree.item(selected_item)['values'][0]
+        prenom = prenom_var.get() if prenom_var.get() else tree.item(selected_item)['values'][1]
+        nom = nom_var.get() if nom_var.get() else tree.item(selected_item)['values'][2]
+        email = email_var.get() if email_var.get() else tree.item(selected_item)['values'][3]
+        tel = telephone_var.get() if telephone_var.get() else tree.item(selected_item)['values'][4]
+        photo = photo_var.get() if photo_var.get() else tree.item(selected_item)['values'][5]
+        print("Prenom:{} Nom:{}" .format(prenom_var.get(), nom_var.get()))
+        print("Prenom:{} Nom:{}" .format(tree.item(selected_item)['values'][1], tree.item(selected_item)['values'][2]))
+        # Update the contact in the database
+        update_contact(contact_id, prenom, nom, email, tel, photo)
+        update_contacts_list()   
+        clear_fields()
 
 def rechercher_contact():
-    pass
+    nom = nom_var.get()
+    prenom = prenom_var.get()
+    email = email_var.get()
+    tel = telephone_var.get()
+    recherche = search_contact(prenom, nom, email, tel)
+    print("oui")
 
-def clear_fields():
-    pass
+    if recherche:
+        print(recherche)
 
 def browse_photo():
     pass
@@ -55,13 +91,16 @@ telephone_var = ctk.StringVar()
 photo_var = ctk.StringVar()
 
 
-tree = ttk.Treeview(columns=("Nom", "Prénom", "Email", "Téléphone", "Photo"), show="headings", height=10, master=tabview.tab("Contacts") )
+tree = ttk.Treeview(columns=("ID","Nom", "Prénom", "Email", "Téléphone", "Photo"), show="headings", height=10, master=tabview.tab("Contacts") )
+tree.heading("ID", text="ID")
 tree.heading("Nom", text="Nom")
 tree.heading("Prénom", text="Prénom")
 tree.heading("Email", text="Email")
 tree.heading("Téléphone", text="Téléphone")
 tree.heading("Photo", text="Photo")
 tree.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+tree.column("ID", width=15)
 
 contacts_button_frame = ctk.CTkFrame(master=tabview.tab("Contacts"))
 contacts_button_frame.grid(row=1, column=0, pady=5, padx=5, sticky="nsew")
@@ -79,7 +118,7 @@ def update_contacts_list():
 
     # Add each contact to the treeview
     for contact in contacts:
-        tree.insert('', 'end', values=(contact[1], contact[2], contact[3], contact[4], contact[5]))
+        tree.insert('', 'end', values=(contact[0],contact[1], contact[2], contact[3], contact[4], contact[5]))
 update_contacts_list()
 
 fields_frame = ctk.CTkFrame(master=tabview.tab("Rechercher"))
