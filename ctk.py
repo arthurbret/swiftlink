5e61316aa0a63595912f20af10b7822639890c5f
Arthur <arthur.bret@edu.ece.fr>
2023-12-19 12:03:35 +0100

    Ajout fonction recherche et affichage photo

--- ctk.py
+++ ctk.py
@@ -1,6 +1,9 @@
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
 from SQLite_func import create_contact
 from SQLite_func import delete_contact
 from SQLite_func import update_contact
@@ -10,8 +13,8 @@ from SQLite_func import get_all_contacts
 ctk.set_default_color_theme("dark-blue")
 
 
-def change_tab_Rechercher():
-    tabview.set("Rechercher")
+def change_tab_action():
+    tabview.set("Actions")
 
 def clear_fields():
     prenom_var.set("")
@@ -49,8 +52,6 @@ def modifier_contact():
         email = email_var.get() if email_var.get() else tree.item(selected_item)['values'][3]
         tel = telephone_var.get() if telephone_var.get() else tree.item(selected_item)['values'][4]
         photo = photo_var.get() if photo_var.get() else tree.item(selected_item)['values'][5]
-        print("Prenom:{} Nom:{}" .format(prenom_var.get(), nom_var.get()))
-        print("Prenom:{} Nom:{}" .format(tree.item(selected_item)['values'][1], tree.item(selected_item)['values'][2]))
         # Update the contact in the database
         update_contact(contact_id, prenom, nom, email, tel, photo)
         update_contacts_list()   
@@ -62,34 +63,67 @@ def rechercher_contact():
     email = email_var.get()
     tel = telephone_var.get()
     recherche = search_contact(prenom, nom, email, tel)
-    print("oui")
-
+    print("Recherche en cours...")
     if recherche:
-        print(recherche)
-
+        print(recherche)            
+        tabview.set("Résultats")  # Configuration de l'onglet "Résultats" comme onglet actif
+        tree_results = ttk.Treeview(columns=("ID","Prénom", "Nom", "Email", "Téléphone", "Photo"), show="headings", height=10, master=tabview.tab("Résultats") )
+        tree_results.heading("ID", text="ID")
+        tree_results.heading("Prénom", text="Prénom")
+        tree_results.heading("Nom", text="Nom")
+        tree_results.heading("Email", text="Email")
+        tree_results.heading("Téléphone", text="Téléphone")
+        tree_results.heading("Photo", text="Photo")
+        tree_results.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+        tree_results.column("ID", width=15)
+        for contact in recherche:
+            tree_results.insert('', 'end', values=(contact[0],contact[1], contact[2], contact[3], contact[4], contact[5]))
+    else:
+        pass
+    clear_fields()
+        
 def browse_photo():
-    pass
+    selected_item = tree.selection()
+    if selected_item:
+        tabview.set("Photo")  # Configuration de l'onglet "Photo" comme onglet actif
+        url = tree.item(selected_item)['values'][5]
+        print(url)
 
+        # Récupérer l'image depuis l'URL
+        reponse = requests.get(url)
+        img_data = reponse.content
+        img = Image.open(BytesIO(img_data))
+
+        # Convertir l'image en format tkinter
+        tk_img = ImageTk.PhotoImage(img)
+
+        # Afficher l'image dans le label
+        label.config(image=tk_img)
+        label.image = tk_img
+        label.pack()
+
+# Création de la fenêtre
 fenetre = ctk.CTk(fg_color="#6F8FAF")
 fenetre.title("Swiftlink") # Titre de la fenêtre
 fenetre.iconbitmap("logo.ico") # Logo de la fenêtre
 
-
+# Création du tabview et des onglets
 tabview = ctk.CTkTabview(master=fenetre)
 tabview.pack(padx=20, pady=20)
+tabview.add("Contacts")  # Ajout d'un onglet "Contacts"
+tabview.add("Actions")  # Ajout d'un onglet "Actions"
+tabview.add("Résultats")  # Ajout d'un onglet "Résultats"
+tabview.add("Photo")  # Ajout d'un onglet "Photo"
+tabview.set("Contacts")  # Configuration de l'onglet "Contacts" comme onglet actif
 
-tabview.add("Contacts")  # add tab at the end
-tabview.add("Rechercher")  # add tab at the end
-tabview.set("Contacts")  # set currently visible tab
-
-
+# Création des widgets champs de saisie
 nom_var = ctk.StringVar()
 prenom_var = ctk.StringVar()
 email_var = ctk.StringVar()
 telephone_var = ctk.StringVar()
 photo_var = ctk.StringVar()
 
-
+# Création du treeview
 tree = ttk.Treeview(columns=("ID","Prénom", "Nom", "Email", "Téléphone", "Photo"), show="headings", height=10, master=tabview.tab("Contacts") )
 tree.heading("ID", text="ID")
 tree.heading("Prénom", text="Prénom")
@@ -98,14 +132,16 @@ tree.heading("Email", text="Email")
 tree.heading("Téléphone", text="Téléphone")
 tree.heading("Photo", text="Photo")
 tree.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
-
 tree.column("ID", width=15)
 
 contacts_button_frame = ctk.CTkFrame(master=tabview.tab("Contacts"))
 contacts_button_frame.grid(row=1, column=0, pady=5, padx=5, sticky="nsew")
 
 ctk.CTkButton(contacts_button_frame, text="Supprimer", command=supprimer_contact).grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
-ctk.CTkButton(contacts_button_frame, text="Modifier", command=change_tab_Rechercher).grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
+ctk.CTkButton(contacts_button_frame, text="Modifier", command=change_tab_action).grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
+ctk.CTkButton(contacts_button_frame, text="Voir photo", command=browse_photo).grid(row=0, column=3, padx=5, pady=5, sticky="nsew")
+
+label = tk.Label(tabview.tab("Photo"))
 
 def update_contacts_list():
     # Clear the old contacts from the treeview
@@ -120,7 +156,7 @@ def update_contacts_list():
         tree.insert('', 'end', values=(contact[0],contact[1], contact[2], contact[3], contact[4], contact[5]))
 update_contacts_list()
 
-fields_frame = ctk.CTkFrame(master=tabview.tab("Rechercher"))
+fields_frame = ctk.CTkFrame(master=tabview.tab("Actions"))
 fields_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
 ctk.CTkLabel(fields_frame, text="Nom:").grid(row=0, column=0, sticky="e")
